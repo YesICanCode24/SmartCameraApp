96c40351fbbf95bd4a6617e5d185d2312f51bad6
Sam <papasamsow24@gmail.com>
2021-12-20 13:51:45 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/samssmartcamara/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.samssmartcamara
+
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.label.ImageLabeling
+import com.google.mlkit.vision.label.defaults.ImageLabelerOptions
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val imageView= findViewById<ImageView>(R.id.imageView)
+        val button= findViewById<Button>(R.id.captureImageButton).setOnClickListener {
+           dispatchTakePictureIntent()
+        }
+    }
+    val REQUEST_IMAGE_CAPTURE = 1
+
+    private fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        } catch (e: ActivityNotFoundException) {
+            // display error state to the user
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            val imageView= findViewById<ImageView>(R.id.imageView).setImageBitmap(imageBitmap)
+
+            val imageOrientationDegrees = 270
+
+            val image = InputImage.fromBitmap(imageBitmap, imageOrientationDegrees)
+
+            val labeler = ImageLabeling.getClient(ImageLabelerOptions.DEFAULT_OPTIONS)
+
+            labeler.process(image)
+                .addOnSuccessListener { labels ->
+                    // Task completed successfully
+                    findViewById<TextView>(R.id.textView).text = labels[0].text
+                }
+                .addOnFailureListener { e ->
+                    // Task failed with an exception
+                    findViewById<TextView>(R.id.textView).text = e.toString()
+                }
+        }
+    }
+}
